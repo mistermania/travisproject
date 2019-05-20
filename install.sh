87773f761827c0a675e1f465b81c43fe0ff3cd5e
mistermania <misterplaymania@gmail.com>
2019-05-20 15:16:13 +0200

    yes

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir ${HOME}/.kube
+cp k3s.yaml ${HOME}/.kube/config
+kubectl config set clusters.default.server "$KUBE_SERVER"
+kubectl config set users.default.username "$KUBE_USERNAME"
+kubectl config set users.default.password "$KUBE_PASSWORD"
+kubectl config set clusters.default.certificate-authority-data "$KUBE_CERT" --set-raw-bytes=false
+
